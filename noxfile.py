06354f6032d472ceac836c0e001029f3c91402ec
dwerkjem <derekrneilson@gmail.com>
2024-12-27 00:15:40 -0700

    chore: add Nox configuration for testing and linting, and include pre-commit hooks

---
+++ noxfile.py
@@ -0,0 +1,25 @@
+import nox
+
+
+@nox.session
+def tests(session):
+    session.install("pytest")
+    session.run("pytest")
+
+
+@nox.session
+def lint(session):
+    session.install("flake8")
+    session.run("flake8", "--import-order-style", "google")
+
+
+@nox.session
+def black(session):
+    session.install("black")
+    session.run("black", "--check", ".")
+
+
+@nox.session
+def isort(session):
+    session.install("isort")
+    session.run("isort", "--check", "--profile", "black", ".")
