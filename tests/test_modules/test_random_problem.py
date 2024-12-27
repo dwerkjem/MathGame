aa3a25354a8115c44bc82e0632608ea07f3472e5
dwerkjem <derekrneilson@gmail.com>
2024-12-27 04:49:55 -0700

    feat: add numpy dependency and implement tests for random value generation in RandomProblem class

--- tests/test_modules/test_random_problem.py
+++ tests/test_modules/test_random_problem.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from math_game.modules import random_problem
 
 
@@ -7,3 +9,31 @@ def test_number_of_zeros():
     assert rp.number_of_zeros(1000) == 3
     assert rp.number_of_zeros(100402) == 3
     assert rp.number_of_zeros(100402000) == 6
+
+
+def test_random_value():
+    rp = random_problem.RandomProblem()
+    values = [rp.random_value() for _ in range(1000)]
+    assert np.mean(values) >= 0
+    assert np.mean(values) <= 20
+    assert np.std(values) > 0
+
+    rp = random_problem.RandomProblem(floats=True)
+    values = [rp.random_value() for _ in range(1000)]
+    assert np.mean(values) >= 0
+    assert np.mean(values) <= 20
+    assert np.std(values) > 0
+    assert any(isinstance(value, float) for value in values)
+
+    rp = random_problem.RandomProblem(max_number=10)
+    values = [rp.random_value() for _ in range(1000)]
+    assert np.mean(values) >= 0
+    assert np.mean(values) <= 10
+    assert np.std(values) > 0
+
+    rp = random_problem.RandomProblem(floats=True, max_number=10)
+    values = [rp.random_value() for _ in range(1000)]
+    assert np.mean(values) >= 0
+    assert np.mean(values) <= 10
+    assert np.std(values) > 0
+    assert any(isinstance(value, float) for value in values)
