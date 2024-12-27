4a6bb3d6df942e260d4c137cb7a0e71c9e0c113d
dwerkjem <derekrneilson@gmail.com>
2024-12-27 00:05:15 -0700

    docs: update Sphinx configuration to use 'haiku' theme and add LaTeX output options

--- docs/conf.py
+++ docs/conf.py
@@ -19,10 +19,21 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'haiku'
 html_static_path = ['_static']
+
+# -- Options for LaTeX output ------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-latex-output
+
+latex_engine = 'pdflatex'
+
+latex_elements = {
+    'papersize': 'letterpaper',
+    'pointsize': '10pt',
+    'figure_align': 'htbp',
+}
+
+text_newlines = 'unix'
